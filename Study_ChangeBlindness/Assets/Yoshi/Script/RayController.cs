9b71aa0041b14f2d2a93f041db0f1758ad5b93e4
Yoshi01O <22211370yy@tama.ac.jp>
2025-12-23 06:30:54 +0900

    Update

---
+++ Study_ChangeBlindness/Assets/Yoshi/Script/RayController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class RayController : MonoBehaviour
+{
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(this.gameObject.transform.position, this.gameObject.transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction * 30, Color.red, 0.1f);
+        Debug.Log($"RayController :  show");
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log($"ray = {ray}, hit = {hit} , hit pos = {hit.point}");
+            Instantiate(target, hit.point, target.transform.rotation);
+        }
+    }
+}
