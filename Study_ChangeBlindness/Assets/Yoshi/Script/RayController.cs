836572ddab61ad407b5ac9df41f231dd4655bd43
Yoshi01O <22211370yy@tama.ac.jp>
2026-01-13 12:53:14 +0900

    Solve erro & work ScreenPointToRay()

--- Study_ChangeBlindness/Assets/Yoshi/Script/RayController.cs
+++ Study_ChangeBlindness/Assets/Yoshi/Script/RayController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RayController : MonoBehaviour
 {
     public GameObject target;
-    public GameObject picture_camera;
+    public Camera picture_camera;
     public Ray ray;
 
     // Start is called before the first frame update
@@ -27,7 +27,7 @@ public class RayController : MonoBehaviour
             Vector3 hit_pos = hit.point;
             Ray picture_ray = picture_camera.ScreenPointToRay(hit_pos);
             Debug.DrawRay(picture_ray.origin, ray.direction * 30, Color.red, 0.1f);
-            Debug.Log($"ray = {picture_ray} , hit pos x,y = {hit_pos.x, hit_pos.y}");
+            Debug.Log($"ray = {picture_ray} , hit pos x,y = {hit_pos.x}, {hit_pos.y}");
 //            Instantiate(target, hit.point, target.transform.rotation);
         }
     }
